a8942dc28d54217031d52f2ec39c88010261237e
Sanjayai006 <ai.interviewer006@gmail.com>
2025-08-02 10:14:20 +0530

    first commits

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '193.203.184.105',
-    user: 'u280225344_69',
-    password: 'u280225344_69U',
-    database: 'u280225344_69',
+    host: '193.203.184.175',
+    user: 'u974440670_jackpot',
+    password: 'U974440670_jackpot',
+    database: 'u974440670_jackpot',
 });
 
 export default connection;
